c167ccf9d490a13fccaf20beacf01653bff071a5
John <john@john-VirtualBox.(none)>
2013-06-20 21:13:52 +0100

    Add rails srtucture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leelol::Application.config.secret_token = '0d8551edea1d24e2441b58d889c958e64851027d74193a0ffe534ef29c849057970bf7bf15cecad5a2b493a3e14683746dfa0ba55f59c13a87a5b5d59c1d0e2e'
