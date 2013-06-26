cb8804b7c65d68b9d9109b5b67c17b1f1ce2d665
xxswingxx <vidadelaempresa@gmail.com>
2013-06-26 15:41:08 +0100

    Fixed issues with weird positions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  layout :layout_by_resource 
+
+  def layout_by_resource 
+    if params[:controller] =~ /devise/
+      "users" 
+    else 
+      "application" 
+    end 
+  end
 end
