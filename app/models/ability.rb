dcd28f433b0084f17ec16385a5897ed2eeda0cff
John <john@john-VirtualBox.(none)>
2013-06-30 19:51:13 +0100

    Added default user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,6 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    debugger
     user ||= User.new # guest user (not logged in)
     if user.admin
       can :manage, :all
